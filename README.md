7829b4ec70ab617ba2dd82e73cb222f95e5ceebb
Oded <odedlevy02@gmail.com>
2019-12-02 23:16:24 +0200

    update readme

--- README.md
+++ README.md
@@ -5,11 +5,21 @@ The project contains 2 services:
 1. Gateway - this is the main service that needs to forward the files to the files server
 2. FilesServer - this is the service that should save the file
 
-This project is based on multer for doing the heavy loading of working with files
-The Gateway will stream the file in memory
+This FilesServer is based on multer for saving the files to disk
+The Gateway will stream the file chunk by chunk
 The File server will save the file to disk
 
-The important thing to note is in the gateway and the creation of the formData object.
-After mapping  the buffer in to the formData you are required to set the name of the buffer. 
-Without this the file will not be passed on
-
+To run the code:
+Gateway:
+```
+cd gateway
+npm i
+tsc
+```
+File server:
+```
+cd fileserver
+npm i
+tsc
+```
+If you are using vscode just select 'run-all' to debug
--- gateway/files/files.router.ts
+++ gateway/files/files.router.ts
@@ -1,10 +1,6 @@
 import { Router } from "express";
 import * as express from "express";
-import * as multer from "multer"
 import * as request from "request"
-import * as fs from "fs";
-import * as path from "path"
-import * as formidable from "formidable"
 
 class FilesRouter {
 
@@ -16,40 +12,15 @@ class FilesRouter {
   }
 
   private createRoutes() {
-    //create multer memory storage and a multer upload
-    let storage = multer.memoryStorage()
-    let upload = multer({ storage })
-
-    this.router.post("/sendFile", upload.single("file"), this.sendFile);
-    this.router.post("/sendFileWithout", this.sendFileWithout);
-    this.router.post("/sendFileFromFolder", this.sendFileFromFolder);
+    this.router.post("/sendFile", this.sendFile);
   }
 
+  
   sendFile = (req, res, next) => {
-    let url = "http://localhost:3001/files/sendFile"
-    //MOST IMPORTANT - set the buffer name before sending
-    req.file.buffer.name = req.file.originalname
-    //Create a formData object with same file fieldname
-    let formData = { [req.file.fieldname]: req.file.buffer, data: req.body.data }
-    request.post({ url, formData }).pipe(res)
-  }
-
-  sendFileWithout = (req, res, next) => {
     let url = "http://localhost:3001/files/sendFile"
     req.pipe(request(url)).pipe(res)
   }
-
-  sendFileFromFolder = (req, res, next) => {
-    //let fullPath = path.join(__dirname,"../../inputs/file1.jpg")
-    let fullPath = path.join(__dirname, "../../inputs/file2.zip")
-    let fileStream = fs.createReadStream(fullPath, { highWaterMark: 1024000 })
-    let url = "http://localhost:3001/files/sendFile"
-    //fileStream.name = req.file.originalname
-    //Create a formData object with same file fieldname
-    let formData = { file: fileStream, data: "{}" }
-    //let formData2={file:{value:fileStream,options:{filename:"filename"}},data:"{}"}
-    request.post({ url, formData }).pipe(res)
-  }
+  
 }
 
 export const filesRouter = new FilesRouter().router;
